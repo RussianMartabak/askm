3f24ae85770f3bb7d907b91816771b634a93a2e1
Zaky <zakyabdurrahman10@gmail.com>
2022-03-13 08:12:02 +0700

    menyelesaikan fitur create pertanyaan dan upload thumbnail

--- app/Http/Controllers/PertanyaanController.php
+++ app/Http/Controllers/PertanyaanController.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use DB; 
 use App\pertanyaan;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class PertanyaanController extends Controller
 {
@@ -40,19 +41,26 @@ class PertanyaanController extends Controller
         $request->validate([
             'judul'       => 'required ',
             'content'     => 'required',
-            'kategori_id' => 'required'
+            'kategori_id' => 'required',
+            'thumbnail' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
 
         ],
         [
             'judul.required'       => 'judul tidak boleh kosong',
             'content.required'     => 'content harus diisi',
-            'kategori_id.required' => 'harus pilih salah satu kategori'
+            'kategori_id.required' => 'harus pilih salah satu kategori',
+            'thumbnail.max:2048'       => 'ukuran file terlalu besar'
         ]
         );
+        $imageName = time() . '.' . $request['thumbnail']->extension();
+        $request['thumbnail']->move(public_path('pertanyaan_img'), $imageName);
+
         $pertanyaan = new pertanyaan;
         $pertanyaan->Judul = $request['judul'];
         $pertanyaan->content = $request['content'];
+        $pertanyaan->user_id = Auth::user()->id;
         $pertanyaan->kategori_id = $request['kategori_id'];
+        $pertanyaan->thumbnail = $imageName;
         $pertanyaan->save();
         Alert::success('Submit Pertanyaan', 'Berhasil Membuat Pertanyaan');
         return redirect('/');
