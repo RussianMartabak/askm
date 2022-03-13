55f38a745c2bb0eeea652bd9e59b1e7ec8622444
Zaky <zakyabdurrahman10@gmail.com>
2022-03-13 08:27:01 +0700

    Edit halaman index Halaman index menampilkan semua pertanyaan dan link ke masing2 pertanyaan

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\kategori;
+use App\pertanyaan;
 
 class IndexController extends Controller
 {
     //
     function Index() {
         $kategori = kategori::all();
-        return view('index', compact('kategori'));
+        $pertanyaan = pertanyaan::all();
+        return view('index', compact('kategori', 'pertanyaan'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,5 +7,12 @@
     <h2 class="text-center">Semua Pertanyaan</h1>
 </div>
     
-    <a href="/pertanyaan/create"><button class="btn btn-primary">Buat Pertanyaan</button></a>
+    <a href="/pertanyaan/create"><button class="btn btn-primary my-2">Buat Pertanyaan</button></a>
+    @foreach ($pertanyaan as $q)
+    <div class="border p-3">
+        <a href="/pertanyaan/{{$q->id}}"><h5>{{$q->Judul}}</h5></a>
+    </div>
+    @endforeach
+    
+    
 @endsection
