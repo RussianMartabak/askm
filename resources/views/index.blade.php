6453ea41c03325fce044f8dff71c15356cbd03ad
Zaky <zakyabdurrahman10@gmail.com>
2022-03-16 13:20:35 +0700

    show date on post

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,13 +4,14 @@
 @endsection
 @section('content')
 <div class="my-3">
-    <h2 class="text-center">Semua Pertanyaan</h1>
+    <h2 class="text-center">All Questions</h1>
 </div>
     
-    <a href="/pertanyaan/create"><button class="btn btn-primary my-2">Buat Pertanyaan</button></a>
+    <a href="/pertanyaan/create"><button class="btn btn-primary my-2">Ask a Question</button></a>
     @foreach ($pertanyaan as $q)
     <div class="border p-3">
         <a href="/pertanyaan/{{$q->id}}"><h5>{{$q->Judul}}</h5></a>
+        <p>Posted on {{$pertanyaan->created_at}}</p>
     </div>
     @endforeach
     
